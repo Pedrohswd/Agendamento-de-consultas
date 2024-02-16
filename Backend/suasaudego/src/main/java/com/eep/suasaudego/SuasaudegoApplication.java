86c4e22f0721be04240acb2c9af4084e48850d1c
Pedro Henrique <pedrohsfwd@gmail.com>
2024-02-16 10:19:11 -0300

    subindo repositório backend

---
+++ Backend/suasaudego/src/main/java/com/eep/suasaudego/SuasaudegoApplication.java
@@ -0,0 +1,13 @@
+package com.eep.suasaudego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuasaudegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuasaudegoApplication.class, args);
+	}
+
+}
---
+++ Backend/suasaudego/src/test/java/com/eep/suasaudego/SuasaudegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eep.suasaudego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuasaudegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
